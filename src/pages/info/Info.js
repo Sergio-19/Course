2ed55374180955e11e81bde46649918394257534
sergio <stender233@gmail.com>
2020-06-03 00:45:25 +0400

    1 commit

---
+++ src/pages/info/Info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './info.scss'
+
+
+export const Info = () =>{
+
+    return(
+        <div className = 'info'>
+        <h3>Currency conversion application</h3>
+        <p>Author: Sergio</p>
+        
+        </div>
+    )
+}
